61e144359dad8bb5a5c83816ecca75aaa808cdfd
José María Mateos <chema@rinzewind.org>
2015-09-27 23:14:02 +0200

    More repetitions, less sampling (but I am not using this at the moment)

--- relevant_variables_earth.R
+++ relevant_variables_earth.R
@@ -5,8 +5,8 @@ load('C:/Users/jmmateos/Dropbox/Yashar_Jose_Mahsa/Springleaf_Marketing_Response/
 train$target <- factor(train$target)
 train[is.na(train)] <- -1 # quick fix
 
-varimps <- lapply(1:200, function(i) {
-    t1 <- train[sample(20000), ]
+varimps <- lapply(1:500, function(i) {
+    t1 <- train[sample(7000), ]
     a <- earth(target ~ ., t1, degree = 2, nfold = 10, trace = 0)
     varimp <- evimp(a)
     print(varimp)
